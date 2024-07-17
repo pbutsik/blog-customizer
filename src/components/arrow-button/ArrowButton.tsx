2773f83cc28ca3186bc7b49350efb45799ad1bb2
pbutsik <p.butsik@gmail.com>
2024-07-17 08:53:02 +0300

    done

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,42 @@
 import arrow from 'src/images/arrow.svg';
+import clsx from 'clsx';
 
 import styles from './ArrowButton.module.scss';
+import { FC } from 'react';
+
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type TArrowButtonProps = {
+	onClose: () => void;
+	isOpen: boolean;
+};
+
+export function ArrowButton({onClose, isOpen}:TArrowButtonProps) {
+	const buttonStyle = clsx({
+		[styles.container]: true,
+		[styles.container_open]: isOpen,
+	});
+
+	const arrowStyle = clsx({
+		[styles.arrow]: true,
+		[styles.arrow_open]: isOpen,
+	});
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={buttonStyle}
+			onClick={onClose}
+		>
+				<img src={arrow}
+				alt='иконка стрелочки'
+				className={arrowStyle}
+				/>
 		</div>
 	);
 };
--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,29 +1,29 @@
-import type { Meta, StoryObj } from '@storybook/react';
+// import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
+// import { Button } from './Button';
 
-const meta: Meta<typeof Button> = {
-	component: Button,
-};
+// const meta: Meta<typeof Button> = {
+// 	component: Button,
+// };
 
-export default meta;
-type Story = StoryObj<typeof Button>;
+// export default meta;
+// type Story = StoryObj<typeof Button>;
 
-export const ButtonStory: Story = {
-	render: () => {
-		return (
-			<>
-				<Button
-					title='Сбросить'
-					type='reset'
-					onClick={() => alert('клик на кнопку сбросить')}
-				/>
-				<Button
-					title='Применить'
-					type='submit'
-					onClick={() => alert('клик на кнопку применить')}
-				/>
-			</>
-		);
-	},
-};
+// export const ButtonStory: Story = {
+// 	render: () => {
+// 		return (
+// 			<>
+// 				<Button
+// 					title='Сбросить'
+// 					type='reset'
+// 					onClick={() => alert('клик на кнопку сбросить')}
+// 				/>
+// 				<Button
+// 					title='Применить'
+// 					type='submit'
+// 					onClick={() => alert('клик на кнопку применить')}
+// 				/>
+// 			</>
+// 		);
+// 	},
+// };
